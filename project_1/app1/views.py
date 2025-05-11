6b104f557a2f95fb36d1932d7131c23ede05671e
Arjun T A <arjunta32@gmail.com>
2025-05-11 14:21:28 +0530

    discord like app schema completed

--- project_1/app1/views.py
+++ project_1/app1/views.py
@@ -1,23 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-
-rooms=[
-    {'id':1, 'name':'arjun', 'age':20},
-    {'id':2, 'name':'anjali','age':20},
-    {'id':3, 'name':'jaya','age':10} 
-]
+# rooms=[
+#     {'id':1, 'name':'arjun', 'age':20},
+#     {'id':2, 'name':'anjali','age':20},
+#     {'id':3, 'name':'jaya','age':10} 
+# ]
 
 def home(request):
+    rooms= Room.objects.all()
     context= {'rooms':rooms}
     return render(request,'app1/home.html', context)
 
 def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id']==int(pk):
-            room=i
-    
+    room= Room.objects.get(id=pk)
     context= {'room':room}
     return render(request,'app1/room.html',context)
 
