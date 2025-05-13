fbb584b40a79e1d47811ea9992e35a1f240ed3bd
Arjun T A <arjunta32@gmail.com>
2025-05-13 17:24:42 +0530

    implemented search functionality

--- project_1/app1/views.py
+++ project_1/app1/views.py
@@ -10,7 +10,9 @@ from .forms  import RoomForm
 # ]
 
 def home(request):
-    rooms= Room.objects.all()
+    q= request.GET.get('q') if request.GET.get('q')!= None else ''
+    rooms= Room.objects.filter(topic__name__icontains=q)
+    
     topics= Topic.objects.all()
     context= {'rooms':rooms, 'topics': topics}
     return render(request,'app1/home.html', context)
