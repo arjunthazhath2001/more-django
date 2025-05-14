9c95e2e078fcd48d136f689f5f47e62edb31a566
Arjun T A <arjunta32@gmail.com>
2025-05-14 12:39:15 +0530

    completed core functionalities

--- project_1/app1/models.py
+++ project_1/app1/models.py
@@ -32,7 +32,11 @@ class Message(models.Model):
     body= models.TextField()
     updated= models.DateTimeField(auto_now=True)
     created= models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering=['-updated','-created']
+
 
     def __str__(self):
-        return f"{self.body[0:50]}-{self.room.name}"
+        return f"{self.body[0:50]}"
 
